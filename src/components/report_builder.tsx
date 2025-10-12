fa3a22c93d2d6b2ff8c4fe291146a690b519926b
agath2 <agathayanghi@gmail.com>
2025-10-12 18:27:42 -0400

    feat: Completed Report Builder Component

--- src/components/report_builder.tsx
+++ src/components/report_builder.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useState } from "react"; 
+
 /*
  * This component represents a single chart entry in the report builder. Given
  * that we want this component to be resuable as multiple charts get selected,
@@ -5,7 +8,9 @@
  */
 function ChartEntry() {
     return (
-        <div></div>
+        <div className="mx-10 border-2 border-gray-200 py-2 px-8 rounded-xl">
+            this is one chart entry haha
+        </div>
     );
 }
 
@@ -16,10 +21,33 @@ function ChartEntry() {
  * to download the report/data as a PDF, CSV, or PNG.
  */
 export default function ReportBuilder() {
+    /* variable keeping track of #charts we have */
+    const [count, setCount] = useState(0); //UPDATE LATER
+
     return (
         <div>
             {/* we use components within our JSX similarly to html tags */}
-            <ChartEntry />
+            
+            <div className="h-full mx-96 my-5 px-10 py-10 rounded-l-lg border-4 border-indigo-500">
+                <div className="text-lg font-semibold mb-4">Report Builder</div> {/*main title*/}
+                <div className="text-sm text-gray-500 mb-4">{count} chart added</div> {/*# charts subtitle*/}
+                <hr className="border-gray-200"></hr> {/*line divider*/}
+
+                {/*Chart Section*/}
+                <div className="my-10">
+                    <ChartEntry />
+                </div>
+
+                <hr className="border-gray-200"></hr> {/*line divider*/}
+
+                {/*Footer Buttons */}
+                <div className="mt-5 flex justify-evenly w-full py-2">
+                    <button className="border-2 border-gray-200 py-2 px-8 rounded-full">PDF</button>
+                    <button className="border-2 border-gray-200 py-2 px-8 rounded-full">CSV</button>
+                    <button className="border-2 border-gray-200 py-2 px-8 rounded-full">PNG</button>
+                </div>
+
+            </div>
         </div>
     );
 }
