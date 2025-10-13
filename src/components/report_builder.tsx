00513e64066e07b96eca3bb51741109a768a4496
Brianna Taborda <btaborda04@gmail.com>
2025-10-13 15:08:05 -0400

    Update report_builder.tsx, multiple arguments for chart entries

--- src/components/report_builder.tsx
+++ src/components/report_builder.tsx
@@ -6,10 +6,11 @@ import { useState } from "react";
  * that we want this component to be resuable as multiple charts get selected,
  * think about what props it needs to take as you're designing it.
  */
-function ChartEntry() {
+function ChartEntry({title,chartType}:{title:string, chartType:string}) {
     return (
-        <div className="mx-10 border-2 border-gray-200 py-2 px-8 rounded-xl">
-            this is one chart entry haha
+        <div className="mx-10 border-2 border-gray-200 py-2 px-8 rounded-xl"> {/*needs something to enlargen box*/}
+            <div className="text-base font-semibold">{title}</div> {/*chart entry title*/}
+            <div className="text-sm text-gray-400">{chartType}</div> {/*type of chart subtitle*/}
         </div>
     );
 }
@@ -29,13 +30,15 @@ export default function ReportBuilder() {
             {/* we use components within our JSX similarly to html tags */}
             
             <div className="h-full mx-96 my-5 px-10 py-10 rounded-l-lg border-4 border-indigo-500">
-                <div className="text-lg font-semibold mb-4">Report Builder</div> {/*main title*/}
+                <div className="text-lg font-bold mb-4">Report Builder</div> {/*main title*/}
                 <div className="text-sm text-gray-500 mb-4">{count} chart added</div> {/*# charts subtitle*/}
                 <hr className="border-gray-200"></hr> {/*line divider*/}
 
                 {/*Chart Section*/}
                 <div className="my-10">
-                    <ChartEntry />
+                    <ChartEntry title="Families Housed Over Time" chartType="Line Chart"/>
+                    <ChartEntry title="Children Housed Over Time" chartType="Bar Chart"/>
+                    
                 </div>
 
                 <hr className="border-gray-200"></hr> {/*line divider*/}
