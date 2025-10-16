6858b4bd9ae2cd2744b3fa9f1bc7365587cded14
akwan4884 <anson.i.kwan@gmail.com>
2025-10-16 19:20:45 -0400

    Sharing new status checking/updating functions with Kai

--- src/app/api/users/status/route.ts
+++ src/app/api/users/status/route.ts
@@ -5,6 +5,7 @@ import { db } from "@/lib/db";
 import { userInfo } from "@/lib/schema";
 import {eq} from "drizzle-orm";
 
+
 export async function GET(request: Request) {
 
     /* Uses better auth to grab the session from the request header*/
@@ -21,10 +22,33 @@ export async function GET(request: Request) {
     
     /* Fetch call to SQL database, looks in the entire userInfo table for the info where 
        userInfo.id = curUserId  */
-    const permission = await db
-        .select()
+    const authorized = await db
+        .select({authorized: userInfo.authorized})
         .from (userInfo)
         .where (eq(userInfo.id, curUserId));
 
-    return permission;
+    return authorized;
 }
+
+export async function POST(request: Request) {
+    /* Uses better auth to grab the session from the request header*/
+    const session = await auth.api.getSession({headers: (request.headers)});
+    
+
+    /* If the user isn't authenticated, throw a unauthorized error, status 401*/
+    if (!session) {
+        return Response.json({error: "unauthorized"}, {status: 401});
+    }
+
+    /* Grabs the userid associated with the current session */
+    const curUserId = session.user.id;
+    const new_info = await request.json();
+
+    await db
+        .update(userInfo)
+        .set({authorized: new_info.authorized})
+        .where(eq (userInfo.id, curUserId));
+
+    return Response.json({result: "success"})
+}
+
