091dc97b53c3cc25ac1d24f1b305025dffcdee36
agath2 <agathayanghi@gmail.com>
2025-10-18 16:45:13 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import ReportBuilder from "@/components/report_builder";
-
 export default function Home() {
     return (
         <div className="w-screen h-screen flex flex-row justify-center items-center">
