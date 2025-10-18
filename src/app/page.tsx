504de0c4646f3d1609bb2f1ea1bd75a46a58be77
agath2 <agathayanghi@gmail.com>
2025-10-18 16:44:24 -0400

    Update page.tsx
    returned page.tsx to before

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,8 @@ import ReportBuilder from "@/components/report_builder";
 export default function Home() {
     return (
         <>
-            {/* <div className="w-screen h-screen flex flex-row justify-center items-center">
+            <div className="w-screen h-screen flex flex-row justify-center items-center">
                 <h1 className="text-2xl">Boston's Higher Ground!</h1>
-            </div> */}
-            <div>
-                <ReportBuilder/>
             </div>
         </>
     );
