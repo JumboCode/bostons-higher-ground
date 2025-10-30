cfefbf0ff4d85cac7389b4bf1dad90f7e6c93937
Amitav Nott <58123848+anott03@users.noreply.github.com>
2025-10-30 18:14:02 -0400

    remove testing code from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Navbar from "@/components/navbar";
 export default function Home() {
     return (
         <div className="w-screen h-screen flex flex-row justify-center items-center">
-            <Navbar/>
             <h1 className="text-2xl">Boston's Higher Ground!</h1>
         </div>
 
