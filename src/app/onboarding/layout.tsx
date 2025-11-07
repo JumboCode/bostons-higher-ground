20ef1281809e5854f9263e377d41678d7b198534
Vansh Chavda <vansh.chavda@tufts.edu>
2025-11-06 22:02:48 -0500

    Completed the layout styling for onboarding pages

--- src/app/onboarding/layout.tsx
+++ src/app/onboarding/layout.tsx
@@ -4,32 +4,35 @@ import Image from 'next/image'
 export default function OnboardingLayout({ children, } : { children : React.ReactNode } ) {
     return (
         <html lang="en">
-            <body className="">
-                <div className="min-h-dvh flex flex-col bg-gradient-to-br from-rose-50 to-amber-50">
-                    <header className="p-5">
+            <body>
+                <div className="min-h-dvh flex flex-col bg-gradient-to-br from-pink-50 via-white to-yellow-50">
+                    <header className="p-8">
                         <div className="flex flex-row justify-between">
                             <div className="flex flex-row space-x-4 items-center">
                                 <Image
-                                src= { Logo}
+                                src= { Logo }
                                 alt="Description of image"
-                                width={40}
-                                height={40}
+                                width={50}
+                                height={57}
                                 />
                                 <div>
-                                    <p className="font-semibold text-[#555555] tracking-tight text-lg leading-none">Boston Higher Ground</p>
-                                    <p className="font-extralight text-sm">Internal Dashboard</p>
+                                    <p className="text-[#555555] font-semibold leading-tight text-lg">Boston Higher Ground</p>
+                                    <p className="text-[#555555] font-extralight text-sm">Internal Dashboard</p>
                                 </div>
                             </div>
                             <div>
-                                <button className="border border-[#E76C82] text-[#E76C82] rounded-xl px-4 py-2 font-semibold text-base">Sign In</button>
+                                <a href="../login">
+                                    <button className="border border-[#E76C82] text-[#E76C82] rounded-xl px-4 py-2 font-semibold text-base">Sign In</button>
+                                </a>
                             </div>
                         </div>
                     </header>
                     <main className="flex flex-1">
                         {children} 
                     </main>
-                    <footer className="p-5">
-                        <p>footer</p>
+                    <footer className="py-4">
+                        <hr className="my-4 h-0.5 border-t-0 bg-neutral-200"/>
+                        <p className="text-center text-[#555555] font-extralight">&copy; 2025 Boston Higher Ground. For authorized staff use only.</p>
                     </footer>
                 </div>
             </body>
--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,11 +1,6 @@
 export default function Page() {
     return (
         <main className="w-full h-full">
-            <h1>Welcome Home!</h1>
-            <h1>Welcome Home!</h1>
-            <h1>Welcome Home!</h1>
-            <h1>Welcome Home!</h1>
-            <h1>Welcome Home!</h1>
         </main>
     );
 }
