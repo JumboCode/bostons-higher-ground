5ca9e747afa86f07de44c0deb7e4dbb4f07022e5
akwan4884 <anson.i.kwan@gmail.com>
2025-11-10 01:55:34 -0500

    Added register user functionality with untested error handling

--- src/lib/usersFunction.ts
+++ src/lib/usersFunction.ts
@@ -5,6 +5,7 @@
 import { db } from "@/lib/db";
 import { userInfo } from "@/lib/schema";
 import {eq} from "drizzle-orm";
+import { BetterAuthError } from "better-auth";
 
 export async function getUserPermission(userId: string) {
     const [result] = 
@@ -14,4 +15,22 @@ export async function getUserPermission(userId: string) {
             .where(eq (userInfo.id, userId))
 
     return result?.permissions;
+}
+
+// Also contains a function handle better auth errors 
+
+export async function handleBetterAuthError(error: BetterAuthError | null) {
+    if (!error) {
+        return null
+    }
+
+    // need to add to this 
+    switch (error.message) {
+        case "USER_ALREADY_EXISTS":
+            return Response.json({error: "User already exists"}, {status: 401});
+        case "INVALID_EMAIL":
+            return Response.json({error: "invalid input"}, {status: 400});
+        default:
+            return Response.json({error: "unknown server error"}, {status: 500});
+    }
 }
