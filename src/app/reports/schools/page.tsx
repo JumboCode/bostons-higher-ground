2b34781571383f1dd49fd6823ffae34b36591b2a
kristyyau <kristy.yau@tufts.edu>
2025-11-17 20:00:42 -0500

    merge

--- src/app/reports/schools/page.tsx
+++ src/app/reports/schools/page.tsx
@@ -1,36 +1,27 @@
-<<<<<<< HEAD
 import FastFactCard from "@/components/FastFactCard";
+import DashboardTop from '@/components/DashboardTop';
+import SchoolFilterBar from '@/components/SchoolFilterBar';
+
 
 
 export default function Schools(){
     return (
+        
         <div className="flex">
+            <DashboardTop pageTitle="Schools Dashboard" />
+            <div className="flex gap-[70px] ml-[100px] mt-[250px]">
+                <FastFactCard title="Homeless Students" body="45" subtext="" bgColor="bg-[#FFE5EA99]"/>
+                <FastFactCard title="Families Housed to Date" body="82%" subtext="" bgColor="bg-[#E0F7F4]"/>
+                <FastFactCard title="Average Wait Time" body="92%" subtext="" bgColor="bg-[#FDF6EC]"/>
+            </div>
+            <SchoolFilterBar /> 
                     
-                    <div className="flex gap-[70px] ml-[100px] mt-[250px]">
-                        <FastFactCard title="Homeless Students" body="45" subtext="" bgColor="bg-[#FFE5EA99]"/>
-                        <FastFactCard title="Families Housed to Date" body="82%" subtext="" bgColor="bg-[#E0F7F4]"/>
-                        <FastFactCard title="Average Wait Time" body="92%" subtext="" bgColor="bg-[#FDF6EC]"/>
-                    </div>
-                    
-                </div>
+        </div>
+        
+            
+
+    
     );
 }
-=======
-import DashboardTop from '@/components/DashboardTop';
-import SchoolFilterBar from '@/components/SchoolFilterBar';
 
-export default function SchoolsPage() {
-  return (
-    <div className="min-h-screen">
-      
-      {}
-      <DashboardTop pageTitle="Schools Dashboard" />
-      
-      {}
-      <SchoolFilterBar /> 
-      
-      
-    </div>
-  );
-}
->>>>>>> cd77ad036f3181f1503b026f2bdfdf4818d8f465
+
