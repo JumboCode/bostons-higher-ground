f356aaa97dcef99025f475e7ef7140b3059c8968
kristyyau <kristy.yau@tufts.edu>
2025-11-17 20:00:42 -0500

    merge

--- src/components/FastFactCard.tsx
+++ src/components/FastFactCard.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 type FastFactCardProps = {
     title: string;
     body: string;
@@ -22,26 +21,3 @@ export default function FastFactCard({ title, body, subtext, bgColor }: FastFact
         </div>
     );
 }
-=======
-import React from 'react';
-
-interface FastFactCardProps {
-  title: string;
-  statistic: string | number;
-  description: string;
-  bgColor: string; 
-}
-
-const FastFactCard: React.FC<FastFactCardProps> = ({ title, statistic, description, bgColor }) => {
-  return (
-    
-    <div className={`p-6 rounded-xl w-72 h-44 ${bgColor}`}>
-      <p className="text-gray-700 font-medium text-lg">{title}</p>
-      <p className="text-5xl font-bold">{statistic}</p>
-      <p className="text-gray-500 text-sm">{description}</p>
-    </div>
-  );
-};
-
-export default FastFactCard;
->>>>>>> cd77ad036f3181f1503b026f2bdfdf4818d8f465
