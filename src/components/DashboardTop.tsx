818ea6a808d507bf5478280f652fb95ddb0266a9
Amitav Nott <58123848+anott03@users.noreply.github.com>
2025-12-07 00:31:23 -0500

    wip

--- src/components/DashboardTop.tsx
+++ src/components/DashboardTop.tsx
@@ -1,5 +1,3 @@
-
-
 import React from 'react';
 import FastFactCard from "@/components/FastFactCard";
 import { RefreshCcw } from 'lucide-react';
@@ -25,8 +23,6 @@ const DashboardTop: React.FC<DashboardTopProps> = ({ pageTitle, title, body, sub
   return (
     <>
     <div className="w-full flex flex-col p-10">
-      
-      {}
       <div className="w-full flex justify-between items-center mb-10">
         <h1 className="text-4xl font-extrabold text-[#555555] gap-8">{pageTitle}</h1>
         <button className="flex items-center border border-[#E76C82] text-[#E76C82] py-2 px-4 rounded-lg transition-colors hover:bg-pink-50">
@@ -36,19 +32,15 @@ const DashboardTop: React.FC<DashboardTopProps> = ({ pageTitle, title, body, sub
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">...</svg>
           <span className="text-lg">Update Data</span>
         </button>
-      
       </div>
-
     </div>
 
     <div className="flex">
-                       
       <div className={`flex gap-[70px] ml-[80px] ${mt}`}>
           <FastFactCard title={title} body={body} subtext={subtext} bgColor={bgColor}/>
           <FastFactCard title={title1} body={body1} subtext={subtext1} bgColor={bgColor1}/>
           <FastFactCard title={title2} body={body2} subtext={subtext2} bgColor={bgColor2}/>
       </div>
-                
     </div>
     </>
   );
