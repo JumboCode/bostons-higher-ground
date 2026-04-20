a4e5f5084b65aae061585421ce4c81c6ac24637f
hande-nk <handenazkavas@gmail.com>
2026-04-20 18:45:13 -0400

    Fixed: Headers Disappearing on Scroll

--- src/app/reports/overview/page.tsx
+++ src/app/reports/overview/page.tsx
@@ -13,11 +13,14 @@ const final_Data = data.map((d) => ({
 export default function Overview() {
     return (
         <div>
-            <div className="sticky top-0 min-h-[40px] bg-white top-0 flex justify-between py-3 drop-shadow-sm z-50">
+            <div className="fixed top-0 left-62.5 w-full bg-white min-h-10 flex justify-between py-3 drop-shadow-sm z-50">
                         <FilterBar />
                         {/* {userName || "John Doe"} */}
             </div>
-            <OverviewClient data={final_Data} />
+            <div className="pt-14">
+                <OverviewClient data={final_Data} />
+            </div>
+            
         </div>
     
     );
